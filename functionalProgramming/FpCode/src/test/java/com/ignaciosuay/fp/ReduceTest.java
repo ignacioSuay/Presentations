440cf51608b367edeabf9637cd6c09bd9b6c0b71
Ignacio SuayMas <Ignacio.SuayMas@kcom.com>
2017-05-03 17:28:24 +0100

    add reduce test

---
+++ functionalProgramming/FpCode/src/test/java/com/ignaciosuay/fp/ReduceTest.java
@@ -0,0 +1,34 @@
+package com.ignaciosuay.fp;
+
+import javaslang.collection.Stream;
+import org.junit.Test;
+
+
+/**
+ * Created by suaymasi on 03/05/2017.
+ */
+public class ReduceTest {
+
+    @Test
+    public void reduceTest() {
+        Integer result = Stream.of(5, 2, 3)
+                .reduce((x, y) -> x + y);
+        assert result == 10;
+    }
+
+    @Test
+    public void foldLeftTest() {
+        Integer result = Stream.of(5, 2, 3)
+                .fold(0, (x, y) -> x + y);
+        assert result == 10;
+    }
+
+    @Test
+    public void foldRightTest() {
+        Integer result = Stream.of(5, 2, 3)
+                .foldRight(0, (x, y) -> x + y);
+        assert result == 10;
+    }
+
+
+}
