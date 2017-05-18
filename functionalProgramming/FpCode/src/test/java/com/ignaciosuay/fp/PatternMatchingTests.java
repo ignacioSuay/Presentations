113f096eeb181feb90ee872f73e04974b9797530
Ignacio Suay <ignacio.suay@gmail.com>
2017-05-18 17:40:47 +0100

    update pattern matching tests

--- functionalProgramming/FpCode/src/test/java/com/ignaciosuay/fp/PatternMatchingTests.java
+++ functionalProgramming/FpCode/src/test/java/com/ignaciosuay/fp/PatternMatchingTests.java
@@ -1,8 +1,13 @@
 package com.ignaciosuay.fp;
 
 
+import com.ignaciosuay.fp.model.Person;
+import com.ignaciosuay.fp.validation.PersonValidator;
+import io.vavr.collection.List;
+import io.vavr.collection.Seq;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
+import io.vavr.control.Validation;
 import org.junit.Test;
 
 import static io.vavr.API.*;
@@ -25,23 +30,36 @@ public class PatternMatchingTests {
     }
 
     @Test
-    public void testDataPM() {
+    public void testTryPM() {
         Try<Integer> divideBy0 = Try.of(() -> 1 / 0);
-
         Integer a = Match(divideBy0).of(
                 Case($Success($()), x -> 1),
                 Case($Failure($()), x -> 0)
         );
-        System.out.println(a);
+        assert a == 0;
+    }
 
+    @Test
+    public void testOptionPM() {
         Option option = Option.of(1);
-
-        System.out.println(Match(option).of(
-                Case($Some($(1)), () -> "defined1"),
-                Case($Some($()), () -> "defined"),
+        String optionResult = Match(option).of(
+                Case($Some($(1)), () -> "is 1"),
+                Case($Some($()), () -> "has a value but it is not 1"),
                 Case($None(), () -> "empty")
-        ));
+        );
+        assert optionResult.equals("is 1");
+    }
 
+    @Test
+    public void testDatePM() {
 
+        PersonValidator personValidator = new PersonValidator();
+        Validation<Seq<String>, Person> suay = personValidator.validatePerson("suay", -1);
+        String validationResult = Match(suay).of(
+                Case($Valid($()), "valid"),
+                Case($Invalid($(List.of("Age must be between 0 and 150"))), "invalid age"),
+                Case($Invalid($()), "any other error")
+        );
+        assert validationResult.equals("invalid age");
     }
 }
